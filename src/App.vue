8a706316e1fce05ebc800dc430cba1f7dbaaf46a
wxwzl <zgkj456@163.com>
2021-09-25 14:47:22 +0800

    chore: 增加css-format

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,11 @@
 
 <style lang="less">
   #app {
+    height: 100%;
+    color: #2c3e50;
     font-family: MicrosoftYaHei;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    color: #2c3e50;
-    height: 100%;
   }
 </style>
