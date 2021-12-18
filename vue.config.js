d15fa41574ec21a5c57221fffebdc40df7ecf94e
wxwzl <zgkj456@163.com>
2021-12-18 15:23:23 +0800

    chore: 添加多环境打包配置

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 const IS_PROD = ["production", "prod"].includes(process.env.NODE_ENV);
+const IS_LOCALORDEV = ["local", "mock"].includes(process.env.VUE_APP_MODE);
 const path = require("path");
 
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
@@ -65,85 +66,90 @@ module.exports = {
     //   }
     // }
   },
-  configureWebpack: {
-    externals: {
-      vue: "Vue",
-      "element-ui": "ELEMENT",
-      "vue-router": "VueRouter",
-      vuex: "Vuex",
-      axios: "axios",
-    },
-    plugins: [
-      new HardSourceWebpackPlugin({
-        // cacheDirectory是在高速缓存写入。默认情况下，将缓存存储在node_modules下的目录中，因此如
-        // 果清除了node_modules，则缓存也是如此
-        // cacheDirectory: "node_modules/.cache/hard-source/[confighash]",
-        // Either an absolute path or relative to webpack's options.context.
-        // Sets webpack's recordsPath if not already set.
-        // recordsPath: "node_modules/.cache/hard-source/[confighash]/records.json",
-        // configHash在启动webpack实例时转换webpack配置，并用于cacheDirectory为不同的webpack配
-        // 置构建不同的缓存
-        configHash: function (webpackConfig) {
-          // node-object-hash on npm can be used to build this.
-          return require("node-object-hash")({ sort: false }).hash(webpackConfig);
-        },
-        // 当加载器，插件，其他构建时脚本或其他动态依赖项发生更改时，hard-source需要替换缓存以确保输
-        // 出正确。environmentHash被用来确定这一点。如果散列与先前的构建不同，则将使用新的缓存
-        environmentHash: {
-          root: process.cwd(),
-          directories: [],
-          files: ["package-lock.json", "yarn.lock"],
-        },
-      }),
-      new stylelintWebpackPlugin({
-        files: ["src/**/*.{html,vue,css,sass,scss,less}"],
-        extensions: ["vue", "css", "less", "scss", "sass"],
-        caches: true,
-        fix: true,
-        failOnError: true,
-      }),
-      new MultiEnvHtmlWebpackPlugin([
-        {
-          index: "./dist/index.html",
-          outputs: [
-            {
-              data: { [`${globalEnvName}`]: getEnv(process.env.mode) },
-              file: "./dist/index.html",
-            },
-            {
-              data: { [`${globalEnvName}`]: getEnv("development") },
-              file: "./dist/index-development.html",
-            },
-            {
-              data: { [`${globalEnvName}`]: getEnv("test") },
-              file: "./dist/index-test.html",
-            },
-            {
-              data: { [`${globalEnvName}`]: getEnv("production") },
-              file: "./dist/index-production.html",
-            },
-            {
-              data: { [`${globalEnvName}`]: getEnv("uat") },
-              file: "./dist/index-uat.html",
-            },
-          ],
-        },
-      ]),
-    ],
-    // entry: "./src/main.ts",
-    // resolve:{
-    //   extensions:[".js", ".vue", ".json",".ts"],
-    // }
+  configureWebpack: () => {
+    let config = {
+      externals: {
+        vue: "Vue",
+        "element-ui": "ELEMENT",
+        "vue-router": "VueRouter",
+        vuex: "Vuex",
+        axios: "axios",
+      },
+      plugins: [
+        new HardSourceWebpackPlugin({
+          // cacheDirectory是在高速缓存写入。默认情况下，将缓存存储在node_modules下的目录中，因此如
+          // 果清除了node_modules，则缓存也是如此
+          // cacheDirectory: "node_modules/.cache/hard-source/[confighash]",
+          // Either an absolute path or relative to webpack's options.context.
+          // Sets webpack's recordsPath if not already set.
+          // recordsPath: "node_modules/.cache/hard-source/[confighash]/records.json",
+          // configHash在启动webpack实例时转换webpack配置，并用于cacheDirectory为不同的webpack配
+          // 置构建不同的缓存
+          configHash: function (webpackConfig) {
+            // node-object-hash on npm can be used to build this.
+            return require("node-object-hash")({ sort: false }).hash(webpackConfig);
+          },
+          // 当加载器，插件，其他构建时脚本或其他动态依赖项发生更改时，hard-source需要替换缓存以确保输
+          // 出正确。environmentHash被用来确定这一点。如果散列与先前的构建不同，则将使用新的缓存
+          environmentHash: {
+            root: process.cwd(),
+            directories: [],
+            files: ["package-lock.json", "yarn.lock"],
+          },
+        }),
+        new stylelintWebpackPlugin({
+          files: ["src/**/*.{html,vue,css,sass,scss,less}"],
+          extensions: ["vue", "css", "less", "scss", "sass"],
+          caches: true,
+          fix: true,
+          failOnError: true,
+        }),
+      ],
+    };
+    if (!IS_LOCALORDEV) {
+      config.plugins.push(
+        new MultiEnvHtmlWebpackPlugin([
+          {
+            index: "./dist/index.html",
+            outputs: [
+              {
+                data: { [`${globalEnvName}`]: getEnv("development") },
+                file: "./dist/index-development.html",
+              },
+              {
+                data: { [`${globalEnvName}`]: getEnv("test") },
+                file: "./dist/index-test.html",
+              },
+              {
+                data: { [`${globalEnvName}`]: getEnv("production") },
+                file: "./dist/index-production.html",
+              },
+              {
+                data: { [`${globalEnvName}`]: getEnv("uat") },
+                file: "./dist/index-uat.html",
+              },
+              {
+                data: { [`${globalEnvName}`]: getEnv(process.env.mode) },
+                file: "./dist/index.html",
+              },
+            ],
+          },
+        ])
+      );
+    }
   },
   chainWebpack(config) {
     config.entry("app").clear().add("./src/main.ts");
-    config.plugin("define").tap(() => {
-      return [
-        {
-          "process.env": `window.${globalEnvName}`,
-        },
-      ];
-    });
+    if (!IS_LOCALORDEV) {
+      config.plugin("define").tap(() => {
+        return [
+          {
+            "process.env": `window.${globalEnvName}`,
+          },
+        ];
+      });
+    }
+
     // provide the app's title in webpack's name field, so that
     // it can be accessed in index.html to inject the correct title.
     config.set("name", process.env.VUE_APP_APPNAME);
